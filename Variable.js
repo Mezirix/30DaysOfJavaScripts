0116c198d97af1dc9ff4cee8c280d59e8118fd63
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-07-24 14:07:52 -0400

    Task4.js

---
+++ Variable.js
@@ -0,0 +1,7 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let FirstName = 'Anyanwu' // First name of a person
+let LastName = 'Chimeziri' // Last name of a person
+let Country = 'Nigeria' // country
+let city = 'Port-harcourt' // capital city
+let age = 100 // age in years
+let isMarried= False 
