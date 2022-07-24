56b7af9d33975337deb61a00b46ae5c2e0dcc4aa
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-07-24 14:24:29 -0400

    Task8.js

---
+++ Task8.js
@@ -0,0 +1,7 @@
+// Declare variables toStore Your FirstName, lastName, marital Status, country And Age in multipleLines
+let FirstName = 'Anyanwu' // First name of a person
+let LastName = 'Chimeziri' // Last name of a person
+let Country = 'Nigeria' // country
+let city = 'Port-harcourt' // capital city
+let age = 100 // age in years
+let isMarried= False 
