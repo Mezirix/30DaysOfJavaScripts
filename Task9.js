dd594ecf7f58c325297d506fe8976e1b88a8559d
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-07-25 04:13:43 -0400

    Task9.js

---
+++ Task9.js
@@ -0,0 +1,3 @@
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstname = "Chimeziri"; let lastname = "Anyanwu"; let married = "false"; let country = "Nigeria"; let age = "25"
