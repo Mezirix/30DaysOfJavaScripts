48fe62c8e0cbe70dbac32bd4f1ac4b748654dd37
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-07-31 21:35:37 -0400

    Task1L3.js

---
+++ Day2/Level 1/Level 2 Exercises/Level 3 . Exercises/Task1.js
@@ -0,0 +1,7 @@
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love
+
+let string = 'Love is the best thing in this world. Some found their love and some are still looking for their love'
+console.log(string.match('love'))
+
+let pattern= /love/gi
+console.log(string.love(pattern))
---
+++ Day2/Level 1/Level 2 Exercises/Task12.js
@@ -0,0 +1,3 @@
+// Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let because = 'You cannot end a sentence with because because because is a conjunction';
+console.log(because.substring(30, 24));
