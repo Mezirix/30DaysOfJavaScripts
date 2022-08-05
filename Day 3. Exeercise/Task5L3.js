c8c56654548743398079dc046ca243593916e1d8
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-08-05 09:26:55 -0400

    Day 3. Exeercise/Task5L3.js

---
+++ Day 3. Exeercise/Task5L3.js
@@ -0,0 +1,12 @@
+// Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+4 > 3 // True
+4 >= 3 // True
+4 < 3 // False
+4 <= 3 // false
+4 == 4 // True
+4 === 4 // True
+4 != 4 // False
+4 !== 4 // False
+4 != '4' // False
+4 == '4' // True
+4 === '4' // True
