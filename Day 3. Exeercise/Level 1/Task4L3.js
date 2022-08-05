f7530358c05a5d3dd1cc7bd606dfe71f8ffc63a0
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-08-05 08:58:12 -0400

    Day 3. Exercise/Level1 /Track1L3.js

---
+++ Day 3. Exeercise/Level 1/Task4L3.js
@@ -0,0 +1,9 @@
+// Write three JavaScript statement which provide truthy value.
+// Write three JavaScript statement which provide falsy value.
+
+let truvalue = 7 < 4;
+let isMarried = true
+*/let truvalue = 10 < 4;*/
+
+let falsy= 4 > 10;
+// let falsy= isHungry;
