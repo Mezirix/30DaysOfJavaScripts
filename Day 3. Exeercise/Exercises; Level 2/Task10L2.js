c1e3be9611eeeac34d342044d9101ab5ad00a381
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-08-11 06:35:13 -0400

    Task10L2.js

---
+++ Day 3. Exeercise/Exercises; Level 2/Task10L2.js
@@ -0,0 +1,3 @@
+// If the length of your name is greater than 7 say, your name is long else say your name is short.
+
+// name.lenght > 7;  console.log('my name is long); : console.log('my name is short')
