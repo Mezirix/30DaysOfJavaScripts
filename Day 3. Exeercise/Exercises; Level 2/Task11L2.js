4f61662c444051d525f2c1be7158ced87ffa3f54
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-08-11 06:44:05 -0400

    Task11L2.js

---
+++ Day 3. Exeercise/Exercises; Level 2/Task11L2.js
@@ -0,0 +1,6 @@
+// Compare your first name length and your family name length and you should get this output.
+// let firstName = 'Asabeneh'
+// let lastName = 'Yetayeh'
+// FirstName, 'Asabeneh' is longer than lastName, 'Yetayeh'
+
+// FirstName.length, LastName.length
