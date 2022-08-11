a57b97ec83f557406bd809d575da4bcc31873be5
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-08-11 05:00:40 -0400

    Task3L2.js

---
+++ Day 3. Exeercise/Exercises; Level 2/Task3L2.js
@@ -0,0 +1,5 @@
+// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+prompt('lenght');
+prompt('width');
+area = length * Width
+perimeter = 2('length + width');
