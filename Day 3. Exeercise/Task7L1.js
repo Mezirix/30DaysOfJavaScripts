29b8e71662ae7d6c4ed50e5a8a304904dfa3e71b
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-08-11 04:02:50 -0400

    Task7L1.js

---
+++ Day 3. Exeercise/Task7L1.js
@@ -0,0 +1,14 @@
+// Use the Date object to do the following activities
+/*What is the year today?
+* What is the month today as a number?
+* What is the date today?
+* What is the day today as a number?
+* What is the hours now?
+* What is the minutes now?
+* Find out the numbers of seconds elapsed from January 1, 1970 to now.*/
+1. // const year = now.getFullYear();
+2. // const month = now.getMonth();
+3. // const date = now.getDate();
+4. // const now = new Date();
+5. // const hours = now.getHours();
+6. // const minutes = now.getMinutes();
